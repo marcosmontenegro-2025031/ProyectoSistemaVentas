2ec17aebc96966e5eae28ee2b49a09f9bc76992d
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-14 11:52:52 -0600

    feat: Se agregaron funcionalidades a UsuarioServiceImplements

--- SistemaVentas/src/main/java/com/marcosmontenegro/Service/UsuarioServiceImplements.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Service/UsuarioServiceImplements.java
@@ -1,4 +1,45 @@
 package com.marcosmontenegro.Service;
 
-public class UsuarioServiceImplements {
+import com.marcosmontenegro.Entity.Usuario;
+import com.marcosmontenegro.Repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class UsuarioServiceImplements implements UsuarioService {
+
+    @Autowired
+    private UsuarioRepository repo;
+
+    @Override
+    public Usuario registrar(String username, String password) {
+        if (repo.findByUsername(username) != null) {
+            return null;
+        }
+        Usuario u = new Usuario();
+        u.setUsername(username);
+        u.setPassword(password);
+        return repo.save(u);
+    }
+
+    @Override
+    public Usuario login(String username, String password) {
+        Usuario u = repo.findByUsername(username);
+        if (u != null && u.getPassword().equals(password)) {
+            return u;
+        }
+        return null;
+    }
+
+    @Override
+    public List<Usuario> getAllUsuarios() {
+        return repo.findAll();
+    }
+
+    @Override
+    public void deleteUsuario(Integer id) {
+        repo.deleteById(id);
+    }
 }
