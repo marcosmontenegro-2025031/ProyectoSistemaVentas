c2d75cd899f8efe7149badd6254a2ee270a3e03f
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-14 07:53:28 -0600

    feat: Se corrigieron agrego una funcionalidad en UsuarioRepository.html

--- SistemaVentas/src/main/java/com/marcosmontenegro/Repository/UsuarioRepository.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Repository/UsuarioRepository.java
@@ -17,4 +17,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
             String rol,
             Integer estado
     );
+
+    Usuario findByUsuario(String usuario);
 }
