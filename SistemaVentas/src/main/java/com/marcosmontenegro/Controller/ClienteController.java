f95268cd703f628ea3e0c94a9d2a6e6e55e9a08b
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:05:18 -0600

    feat: Se añadieron funcionalidades al ClienteController

--- SistemaVentas/src/main/java/com/marcosmontenegro/Controller/ClienteController.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Controller/ClienteController.java
@@ -1,45 +1,58 @@
 package com.marcosmontenegro.Controller;
 
 import com.marcosmontenegro.Entity.Cliente;
+import com.marcosmontenegro.Entity.Usuario;
 import com.marcosmontenegro.Service.ClienteService;
+import com.marcosmontenegro.Service.UsuarioService;
 import com.marcosmontenegro.Exception.ErrorResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/clientes")
+@Controller
 public class ClienteController {
 
     @Autowired
     private ClienteService service;
 
-    @GetMapping
-    public List<Cliente> getAll() {
-        return service.getAllClientes();
+    @GetMapping("/clientes")
+    public String listarClientes(Model model) {
+        List<Cliente> lista = service.getAllClientes();
+        model.addAttribute("clientes", lista);
+        return "clientes";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Cliente> getById(@PathVariable Integer id) {
-        return ResponseEntity.ok(service.getClienteById(id));
+    @GetMapping("/clientes/agregarcliente")
+    public String formularioNuevo(Model model) {
+        model.addAttribute("cliente", new Cliente());
+        return "agregarcliente";
     }
 
-    @PostMapping
-    public ResponseEntity<Cliente> save(@Valid @RequestBody Cliente cliente) {
-        return ResponseEntity.ok(service.saveCliente(cliente));
+    @PostMapping("/clientes/guardarcliente")
+    public String guardarCliente(@ModelAttribute Cliente cliente) {
+        service.saveCliente(cliente);
+        return "redirect:/clientes";
+    }
+    
+
+    @GetMapping("/clientes/editarcliente/{id}")
+    public String formularioEditar(@PathVariable Integer id, Model model) {
+        Cliente cliente = service.getClienteById(id);
+        model.addAttribute("cliente", cliente);
+        return "editarcliente"; 
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Cliente> update(@PathVariable Integer id, @Valid @RequestBody Cliente cliente) {
-        return ResponseEntity.ok(service.updateCliente(id, cliente));
+    @GetMapping("/clientes/eliminar/{id}")
+    public String eliminarCliente(@PathVariable Integer id) {
+        service.deleteCliente(id);
+        return "redirect:/clientes";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<ErrorResponse> delete(@PathVariable Integer id) {
-    service.deleteCliente(id);
-    return ResponseEntity.ok(new ErrorResponse("Cliente eliminado correctamente"));
-    }
 }
+
+
