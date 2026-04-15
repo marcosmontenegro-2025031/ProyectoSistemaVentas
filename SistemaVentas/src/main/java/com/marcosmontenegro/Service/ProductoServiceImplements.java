fbcefe3327c9b43449083f474d5b1878a1a8b5a1
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:12:32 -0600

    feat: Se añadieron funcionalidades al ProductosServiceImplements

--- SistemaVentas/src/main/java/com/marcosmontenegro/Service/ProductoServiceImplements.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Service/ProductoServiceImplements.java
@@ -6,69 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductoServiceImplements implements ProductoService {
 
     @Autowired
-    private ProductoRepository repository;
+    private ProductoRepository repo;
 
     @Override
     public List<Producto> getAllProductos() {
-        return repository.findAll();
+        return repo.findAll();
     }
 
     @Override
     public Producto getProductoById(Integer id) {
-        Optional<Producto> resultado = repository.findById(id);
-        if (resultado.isPresent()) {
-            return resultado.get();
-        } else {
-            throw new RuntimeException("Producto no encontrado con ID: " + id);
-        }
+        return repo.findById(id).orElse(null);
     }
 
     @Override
-    public Producto saveProducto(Producto producto) {
-        boolean existe = repository.existsByNombreAndPrecioAndStockAndEstado(
-                producto.getNombre(),
-                producto.getPrecio(),
-                producto.getStock(),
-                producto.getEstado()
-        );
-
-        if (existe) {
-            throw new RuntimeException("El producto ya existe con estos datos.");
-        }
-
-        return repository.save(producto);
-    }
-
-    @Override
-    public Producto updateProducto(Integer id, Producto actualizado) {
-        Optional<Producto> resultado = repository.findById(id);
-
-        if (resultado.isPresent()) {
-            Producto existente = resultado.get();
-
-            existente.setNombre(actualizado.getNombre());
-            existente.setPrecio(actualizado.getPrecio());
-            existente.setStock(actualizado.getStock());
-            existente.setEstado(actualizado.getEstado());
-
-            return repository.save(existente);
-        } else {
-            throw new RuntimeException("No se puede actualizar: El producto no existe.");
-        }
+    public void saveProducto(Producto producto) {
+        repo.save(producto);
     }
 
     @Override
     public void deleteProducto(Integer id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-        } else {
-            throw new RuntimeException("No se puede deleteUsuario: El producto no existe.");
-        }
+        repo.deleteById(id);
     }
 }
