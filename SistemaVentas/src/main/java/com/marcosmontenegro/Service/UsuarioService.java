1b53bb5d78a871d04208b1642b4ffcca41f0cba8
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:13:08 -0600

    feat: Se añadieron funcionalidades al UsuarioService

--- SistemaVentas/src/main/java/com/marcosmontenegro/Service/UsuarioService.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Service/UsuarioService.java
@@ -7,11 +7,16 @@ public interface UsuarioService {
 
     List<Usuario> getAllUsuarios();
 
+    Usuario getUsuarioById(Integer id);
 
     Usuario registrar(String username, String password);
 
     Usuario login(String username, String password);
 
+    void saveUsuario(Usuario usuario);
+
     void deleteUsuario(Integer id);
+
+    Usuario registrar(String username, String password, String email);
 
 }
