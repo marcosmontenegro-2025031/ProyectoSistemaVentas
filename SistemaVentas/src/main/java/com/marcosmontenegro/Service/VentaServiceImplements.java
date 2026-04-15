bd4f47e7a8dc33c66d520a713269f53cbc065d57
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:15:16 -0600

    feat: Se añadieron funcionalidades al VentaServiceImplements

--- SistemaVentas/src/main/java/com/marcosmontenegro/Service/VentaServiceImplements.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Service/VentaServiceImplements.java
@@ -14,62 +14,25 @@ import java.util.List;
 public class VentaServiceImplements implements VentaService {
 
     @Autowired
-    private VentaRepository repository;
-
-    @Autowired
-    private ClienteRepository clienteRepository;
-
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    private VentaRepository repo;
 
     @Override
     public List<Venta> getAllVentas() {
-        return repository.findAll();
+        return repo.findAll();
     }
 
     @Override
     public Venta getVentaById(Integer id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Venta no encontrada con ID: " + id));
+        return repo.findById(id).orElse(null);
     }
 
     @Override
-    public Venta saveVenta(Venta venta) {
-        validarRelaciones(venta);
-        if (repository.existsByFechaVentaAndTotalAndEstadoAndClienteAndUsuario(
-                venta.getFechaVenta(), venta.getTotal(), venta.getEstado(), venta.getCliente(), venta.getUsuario())) {
-            throw new RuntimeException("La venta ya existe");
-        }
-        return repository.save(venta);
-    }
-
-    @Override
-    public Venta updateVenta(Integer id, Venta datos) {
-        Venta v = getVentaById(id); // Si no existe, lanza excepción
-        
-        validarRelaciones(datos); // Buscamos cliente y usuario nuevos
-        
-        v.setFechaVenta(datos.getFechaVenta());
-        v.setTotal(datos.getTotal());
-        v.setEstado(datos.getEstado());
-        v.setCliente(datos.getCliente());
-        v.setUsuario(datos.getUsuario());
-
-        return repository.save(v);
+    public void saveVenta(Venta venta) {
+        repo.save(venta);
     }
 
     @Override
     public void deleteVenta(Integer id) {
-        if (!repository.existsById(id)) throw new RuntimeException("Venta no existe");
-        repository.deleteById(id);
-    }
-
-    private void validarRelaciones(Venta venta) {
-        Cliente c = clienteRepository.findById(venta.getCliente().getId())
-                .orElseThrow(() -> new RuntimeException("Cliente no existe"));
-        Usuario u = usuarioRepository.findById(venta.getUsuario().getId())
-                .orElseThrow(() -> new RuntimeException("Usuario no existe"));
-        venta.setCliente(c);
-        venta.setUsuario(u);
+        repo.deleteById(id);
     }
 }
