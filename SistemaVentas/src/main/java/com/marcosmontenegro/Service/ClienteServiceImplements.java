d828bd09717f0f508845b7e06a18060ea422c728
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:10:16 -0600

    feat: Se añadieron funcionalidades al ClientesServiceImplements

--- SistemaVentas/src/main/java/com/marcosmontenegro/Service/ClienteServiceImplements.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Service/ClienteServiceImplements.java
@@ -6,67 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ClienteServiceImplements implements ClienteService {
 
     @Autowired
-    private ClienteRepository repository;
+    private ClienteRepository repo;
 
     @Override
     public List<Cliente> getAllClientes() {
-        return repository.findAll();
+        return repo.findAll();
     }
 
     @Override
     public Cliente getClienteById(Integer id) {
-        Optional<Cliente> resultado = repository.findById(id);
-        if (resultado.isPresent()) {
-            return resultado.get();
-        } else {
-            throw new RuntimeException("Cliente no encontrado con el ID: " + id);
-        }
+        return repo.findById(id).orElse(null);
     }
 
     @Override
-    public Cliente saveCliente(Cliente cliente) {
-        boolean existe = repository.existsByNombreAndApellidoAndDireccion(
-                cliente.getNombre(), 
-                cliente.getApellido(), 
-                cliente.getDireccion()
-        );
-
-        if (existe) {
-            throw new RuntimeException("El cliente ya existe en el sistema.");
-        }
-        
-        return repository.save(cliente);
-    }
-
-    @Override
-    public Cliente updateCliente(Integer id, Cliente actualizado) {
-        Optional<Cliente> resultado = repository.findById(id);
-        
-        if (resultado.isPresent()) {
-            Cliente clienteExistente = resultado.get();
-            clienteExistente.setNombre(actualizado.getNombre());
-            clienteExistente.setApellido(actualizado.getApellido());
-            clienteExistente.setDireccion(actualizado.getDireccion());
-            clienteExistente.setEstado(actualizado.getEstado());
-
-            return repository.save(clienteExistente);
-        } else {
-            throw new RuntimeException("No se puede actualizar: El cliente con ID " + id + " no existe.");
-        }
+    public void saveCliente(Cliente cliente) {
+        repo.save(cliente);
     }
 
     @Override
     public void deleteCliente(Integer id) {
-        if (repository.existsById(id)) {
-            repository.deleteById(id);
-        } else {
-            throw new RuntimeException("No se puede deleteUsuario: El cliente con ID " + id + " no existe.");
-        }
+        repo.deleteById(id);
     }
+
 }
