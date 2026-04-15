54d26a4387470cd74bd1b29518a5fe02e83d1f44
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-15 01:08:41 -0600

    feat: Se añadieron funcionalidades al ClienteRepository

--- SistemaVentas/src/main/java/com/marcosmontenegro/Repository/ClienteRepository.java
+++ SistemaVentas/src/main/java/com/marcosmontenegro/Repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package com.marcosmontenegro.Repository;
 
 import com.marcosmontenegro.Entity.Cliente;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +15,6 @@ public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
             String apellido, 
             String direccion
     );
+
+
 }
